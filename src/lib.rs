8fd7626e0a9121dcf9b43a33984d9f94efe37080
Hadar Galant <19251133+hgalant@users.noreply.github.com>
2021-01-08 06:45:42 +0200

    Mention expect in module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@
 //!
 //! # Usage
 //! For unwrapping [`Result`](Result)s:
-//! - Use [`unwrap_message!`](unwrap_message) to provide context.
+//! - Use [`expect`](expect) / [`expect_fatal`](UnwrapExt::expect_fatal) to report the error with context.
+//! - Use [`unwrap_message!`](unwrap_message) to [`expect`](expect) with formatting.
 //! - Use [`unwrap_format!`](unwrap_format) to have more control over the message's format.
 //! - Use [`unwrap`](unwrap) / [`unwrap_fatal`](UnwrapExt::unwrap_fatal) to report the error when context is provided/obvious.
 //!
