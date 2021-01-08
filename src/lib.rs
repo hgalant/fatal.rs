0dae812a8fa20adfeab8b2e29ad6a14f6eff92ca
Hadar Galant <19251133+hgalant@users.noreply.github.com>
2021-01-08 05:16:44 +0200

    Fix rustdoc link ambiguity

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Utilities for reporting fatal errors and exiting with an error code.
 //!
-//! The behavior in this crate is different than the one in [`panic!`](::std::panic)-based exits,
+//! The behavior in this crate is different than the one in [`panic!`](::std::panic!)-based exits,
 //! in that the ones here are suited for display to end-users, i.e. no "thread `main` panicked at", no backtrace mentions, etc.
 //!
 //! # Usage
